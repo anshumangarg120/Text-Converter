d8e21d6d7517a5ec24acd9ed02523d1f702331e7
anshuman120 <anshumangarg120@gmail.com>
2023-08-20 21:59:23 +0530

    add more function

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,6 +11,17 @@ export default function TextForm(props) {
       let newText = text.toLowerCase();
       setText(newText);
   }
+  const handleSentenceFormatClick = () => {
+    let newText = text.toLowerCase();
+    newText = newText.replace(/(^\w|\.\s\w)/gi, char => char.toUpperCase());
+    setText(newText);
+}
+const handleClearClick =()=>{
+  // console.log("Uppercase was clicked" + text);
+  let newText = '';
+  setText(newText);
+}
+
     const handleOnChange =(event)=>{
         // console.log("On Change");
         setText(event.target.value);
@@ -24,12 +35,15 @@ export default function TextForm(props) {
   <div className="form-group">
     <textarea className="form-control" value={text} onChange={handleOnChange} id="MyBox" rows="7"></textarea>
   </div>
-  <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convhert To UperCase</button>
-  <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convhert To LowerCase</button>
+  <button className="btn btn-primary m-1" onClick={handleUpClick}>Convert To UperCase</button>
+  <button className="btn btn-primary m-1" onClick={handleLoClick}>Convert To LowerCase</button>
+  <button className="btn btn-primary m-1" onClick={handleSentenceFormatClick}>Convert To Sentence Format</button>
+  <button className="btn btn-success m-1" onClick={handleClearClick}>Clear Text</button>
     </div>
     <div className="container my-2">
       <h2>Your Text summary</h2>
-      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p> {text.split(". ").length} Lines, {text.split(" ").length} words and {text.length} characters</p>
+      
       <p>{0.008 * text.split(" ").length }Minuets read</p>
       <h2>Preview</h2>
       <p>{text}</p>
