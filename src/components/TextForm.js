6ef5e74412eaa4f9df7097287ae5cca3747cb5f1
anshuman120 <anshumangarg120@gmail.com>
2023-08-20 21:00:36 +0530

    Add lower case tool

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,19 +6,34 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLoClick =()=>{
+      // console.log("Uppercase was clicked" + text);
+      let newText = text.toLowerCase();
+      setText(newText);
+  }
     const handleOnChange =(event)=>{
         // console.log("On Change");
         setText(event.target.value);
     }
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
     // setText("Enter text here 2");
   return (
-    <div>
+    <>
+    <div className="container">
    <h1>{props.heading} </h1>   
   <div className="form-group">
     <textarea className="form-control" value={text} onChange={handleOnChange} id="MyBox" rows="7"></textarea>
   </div>
-  <button className="btn btn-primary" onClick={handleUpClick}>Convhert To UperCase</button>
+  <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convhert To UperCase</button>
+  <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convhert To LowerCase</button>
     </div>
+    <div className="container my-2">
+      <h2>Your Text summary</h2>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length }Minuets read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   )
 }
