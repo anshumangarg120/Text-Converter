72cf5895a31df8baa13169b6d4547b5a38b9ee7a
anshuman120 <anshumangarg120@gmail.com>
2023-08-27 22:06:54 +0530

    oo

--- src/App.js
+++ src/App.js
@@ -1,47 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import Alert from './components/Alert';
-import { useState } from 'react';
-// import About from './components/About';
+/** @format */
+
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+import { useState } from "react";
+import About from "./components/About";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light'); // Wether dark mode is enable or not
+  const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
-    })
+      type: type,
+    });
     setTimeout(() => {
       setAlert(null);
     }, 1500);
-  }
+  };
+
   const toggleMode = () => {
-    if (mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled", "success")
-    }
-    else {
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
     }
-  }
+  };
+
   return (
-    <>
-      {/* <Navbar title="TextUtils" aboutText="About" /> */}
-      {/* <Navbar/> */}
+    <Router>
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter the text " mode={mode} />
-        {/* <About/> */}
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/"
+            element={
+              <TextForm
+                showAlert={showAlert}
+                heading="Enter the text "
+                mode={mode}
+              />
+            }
+          />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@
 
 import React from "react";
 import PropTypes from "prop-types";
-
+import { Link }  from "react-router-dom";
 export default function Navbar(props) {
   return (
     <nav
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-      <a className="navbar-brand" href="/">
+      <Link className="navbar-brand" to="/">
         {props.title}
-      </a>
+      </Link>
       <button
         className="navbar-toggler"
         type="button"
@@ -24,14 +24,14 @@ export default function Navbar(props) {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="/">
+            <Link className="nav-link" to="/">
               Home <span className="sr-only">(current)</span>
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/">
+            <Link className="nav-link" to="/about">
               {props.aboutText}
-            </a>
+            </Link>
           </li>
         </ul>
         {/* <form className="form-inline my-2 my-lg-0">
